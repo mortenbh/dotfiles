4f7b6ebf7127dcece75770eda7b0fba71fa8f461
Morten Bojsen-Hansen <morten@alas.dk>
2022-12-21 19:54:17 +0000

    Add neovim setup.

---
+++ nvim/.config/nvim/init.lua
@@ -0,0 +1,288 @@
+--
+-- ███╗   ██╗ ███████╗ ██████╗  ██╗   ██╗ ██╗ ███╗   ███╗
+-- ████╗  ██║ ██╔════╝██╔═══██╗ ██║   ██║ ██║ ████╗ ████║
+-- ██╔██╗ ██║ █████╗  ██║   ██║ ██║   ██║ ██║ ██╔████╔██║
+-- ██║╚██╗██║ ██╔══╝  ██║   ██║ ╚██╗ ██╔╝ ██║ ██║╚██╔╝██║
+-- ██║ ╚████║ ███████╗╚██████╔╝  ╚████╔╝  ██║ ██║ ╚═╝ ██║
+-- ╚═╝  ╚═══╝ ╚══════╝ ╚═════╝    ╚═══╝   ╚═╝ ╚═╝     ╚═╝
+--
+-- Morten Bojsen-Hansen <morten@alas.dk>
+-- Loosely based on https://github.com/nvim-lua/kickstart.nvim
+
+vim.o.expandtab = true -- Expand tabs to spaces.
+vim.o.tabstop = 4 -- Set the number of spaces a TAB counts for.
+vim.o.shiftwidth = 0 -- Use tabstop value.
+vim.wo.number = true -- Line numbers.
+vim.o.mouse = 'a' -- Enable the use of mouse clicks in all modes.
+vim.o.scrolloff = 10 -- Minimum number of screen lines to keep above and below the cursor.
+vim.o.cursorline = true -- Gighlight line currently under the cursor.
+vim.o.ignorecase = true -- Ignore case in searches.
+vim.o.smartcase = true -- Overrides ignorecase if search pattern contains upper-case.
+vim.o.backup = true -- Always keep a backup of edited files.
+vim.opt.backupdir:remove(".") -- Remove current directory.
+vim.o.undofile = true -- Persistent undo.
+vim.o.termguicolors = true -- True color support.
+vim.g.mapleader = ',' -- Set <Leader>.
+vim.g.maplocalleader = ',' -- Set <LocalLeader>.
+
+-- Convenience mappings.
+vim.keymap.set('n', 'gk', 'gg', { noremap = true, desc = '[G]o to start of file' })
+vim.keymap.set('n', 'gj', 'G', { noremap = true, desc = '[G]o to end of file' })
+vim.keymap.set('n', 'gh', '^', { noremap = true, desc = '[G]o to start of line' })
+vim.keymap.set('n', 'gl', '$', { noremap = true, desc = '[G]o to end of line' })
+-- vim.keymap.set('n', '<leader>w', ':w<cr>', { noremap = true, silent = true, desc = '[W]rite buffer' })
+-- vim.keymap.set('n', '<leader>d', ':bd<cr>', { noremap = true, silent = true, desc = 'Buffer [d]elete' })
+-- vim.keymap.set('n', '<leader>q', ':qa<cr>', { noremap = true, silent = true, desc = '[Q]uit all' })
+
+-- Bootstrap package manager.
+do
+    local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+    if not vim.loop.fs_stat(lazypath) then
+      vim.fn.system({
+        "git",
+        "clone",
+        "--filter=blob:none",
+        "--single-branch",
+        "https://github.com/folke/lazy.nvim.git",
+        lazypath,
+      })
+    end
+    vim.opt.runtimepath:prepend(lazypath)
+end
+
+require("lazy").setup {
+    -- Language Server Protocol.
+    { 'neovim/nvim-lspconfig',
+        dependencies = {
+            'williamboman/mason.nvim',
+            'williamboman/mason-lspconfig.nvim',
+        },
+        config = function()
+            local servers = {
+                clangd = {},
+                rust_analyzer = {},
+                pyright = {},
+                tsserver = {},
+                sumneko_lua = {
+                    Lua = {
+                        -- Suppress lots of 'Undefined global `vim`' warnings.
+                        diagnostics = { globals = { 'vim' }, },
+                        -- Do not send telemetry data containing a randomized but unique identifier.
+                        telemetry = { enable = false },
+                    }
+                },
+                gopls = {}
+            }
+
+            -- Send additional capabilities supported by nvim-cmp to LSP server.
+            local capabilities = vim.lsp.protocol.make_client_capabilities()
+            capabilities = require('cmp_nvim_lsp').default_capabilities(capabilities)
+
+            --  This function gets run when an LSP connects to a particular buffer.
+            local on_attach = function(_, bufnr)
+                local nmap = function(keys, func, desc)
+                    if desc then desc = 'LSP: ' .. desc end
+                    vim.keymap.set('n', keys, func, { buffer = bufnr, desc = desc })
+                end
+
+                nmap('<leader>rn', vim.lsp.buf.rename, '[R]e[n]ame')
+                nmap('<leader>ca', vim.lsp.buf.code_action, '[C]ode [A]ction')
+
+                nmap('gd', vim.lsp.buf.definition, '[G]oto [D]efinition')
+                nmap('gr', require('telescope.builtin').lsp_references, '[G]oto [R]eferences')
+                nmap('gI', vim.lsp.buf.implementation, '[G]oto [I]mplementation')
+                nmap('<leader>D', vim.lsp.buf.type_definition, 'Type [D]efinition')
+                nmap('<leader>ds', require('telescope.builtin').lsp_document_symbols, '[D]ocument [S]ymbols')
+                -- nmap('<leader>ws', require('telescope.builtin').lsp_dynamic_workspace_symbols, '[W]orkspace [S]ymbols')
+
+                -- See `:help K` for why this keymap
+                nmap('K', vim.lsp.buf.hover, 'Hover Documentation')
+                nmap('<C-k>', vim.lsp.buf.signature_help, 'Signature Documentation')
+
+                -- Lesser used LSP functionality
+                nmap('gD', vim.lsp.buf.declaration, '[G]oto [D]eclaration')
+                -- nmap('<leader>wa', vim.lsp.buf.add_workspace_folder, '[W]orkspace [A]dd Folder')
+                -- nmap('<leader>wr', vim.lsp.buf.remove_workspace_folder, '[W]orkspace [R]emove Folder')
+                -- nmap('<leader>wl', function()
+                --     print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
+                -- end, '[W]orkspace [L]ist Folders')
+
+                -- Create a command `:Format` local to the LSP buffer
+                vim.api.nvim_buf_create_user_command(bufnr, 'Format', function(_)
+                    if vim.lsp.buf.format then
+                        vim.lsp.buf.format()
+                    elseif vim.lsp.buf.formatting then
+                        vim.lsp.buf.formatting()
+                    end
+                end, { desc = 'Format current buffer with LSP' })
+            end
+
+            require("mason").setup()
+            require("mason-lspconfig").setup { ensure_installed = vim.tbl_keys(servers) }
+            require("mason-lspconfig").setup_handlers {
+                function(server)
+                    require('lspconfig')[server].setup {
+                        capabilities = capabilities,
+                        on_attach = on_attach,
+                        settings = servers[server],
+                    }
+                end
+            }
+        end
+    },
+
+    -- Tree sitter support.
+    {
+        'nvim-treesitter/nvim-treesitter',
+        config = function()
+            require('nvim-treesitter.configs').setup {
+                ensure_installed = { 'c', 'cpp', 'go', 'lua', 'python', 'rust', 'typescript', 'help' },
+                highlight = { enable = true, },
+            }
+        end
+    },
+
+    {
+        'nvim-treesitter/nvim-treesitter-context',
+        dependencies = { 'nvim-treesitter/nvim-treesitter' }
+    },
+
+    -- Auto-completion.
+    {
+        'hrsh7th/nvim-cmp',
+        dependencies = {
+            'hrsh7th/cmp-nvim-lsp',
+            'hrsh7th/cmp-path',
+            'hrsh7th/cmp-buffer',
+            'hrsh7th/cmp-cmdline',
+            'L3MON4D3/LuaSnip',
+            'saadparwaiz1/cmp_luasnip',
+        },
+        config = function()
+            local cmp = require('cmp')
+            local snip = require('luasnip')
+
+            cmp.setup {
+                snippet = {
+                    expand = function(args)
+                        snip.lsp_expand(args.body)
+                    end,
+                },
+                mapping = cmp.mapping.preset.insert {
+                    ['<c-space>'] = cmp.mapping.complete(),
+                    ['<cr>'] = cmp.mapping.confirm {
+                        behavior = cmp.ConfirmBehavior.Replace,
+                        select = true,
+                    },
+                    ['<c-j>'] = cmp.mapping(function(fallback)
+                        if cmp.visible() then
+                            cmp.select_next_item()
+                        elseif snip.expand_or_jumpable() then
+                            snip.expand_or_jump()
+                        else
+                            fallback()
+                        end
+                    end, { 'i', 's' }),
+                    ['<c-k>'] = cmp.mapping(function(fallback)
+                        if cmp.visible() then
+                            cmp.select_prev_item()
+                        elseif snip.jumpable(-1) then
+                            snip.jump(-1)
+                        else
+                            fallback()
+                        end
+                    end, { 'i', 's' }),
+                },
+                sources = {
+                    { name = 'nvim_lsp' },
+                    { name = 'buffer' },
+                    { name = 'path' },
+                    { name = 'cmdline' },
+                    { name = 'luasnip' },
+                },
+            }
+        end
+    },
+
+    -- Theme inspired by Atom / VS Code.
+    {
+        'navarasu/onedark.nvim',
+        config = function() require('onedark').load() end
+    },
+
+    -- Fancier statusline.
+    {
+        'nvim-lualine/lualine.nvim',
+        config = function()
+            require('lualine').setup {
+                options = {
+                    icons_enabled = false,
+                    theme = 'onedark',
+                    component_separators = '|',
+                    section_separators = '',
+                },
+            }
+        end
+    },
+
+    -- Telescope is a highly extensible fuzzy finder for Neovim.
+    {
+        'nvim-telescope/telescope.nvim',
+        tag = '0.1.0',
+        dependencies = { 'nvim-lua/plenary.nvim' },
+        config = function()
+            local actions = require('telescope.actions')
+
+            require('telescope').setup {
+                defaults = {
+                    mappings = {
+                        -- Move between selections using the home row.
+                        i = {
+                            ['<C-j>'] = actions.move_selection_next,
+                            ['<C-k>'] = actions.move_selection_previous,
+                        },
+                    },
+                },
+            }
+
+            local builtin = require('telescope.builtin')
+            vim.keymap.set('n', '<leader>sf', builtin.find_files, { desc = '[S]earch [F]iles' })
+            vim.keymap.set('n', '<leader>sh', builtin.help_tags, { desc = '[S]earch [H]elp' })
+            vim.keymap.set('n', '<leader>sw', builtin.grep_string, { desc = '[S]earch current [W]ord' })
+            vim.keymap.set('n', '<leader>sg', builtin.live_grep, { desc = '[S]earch by [G]rep' })
+            vim.keymap.set('n', '<leader>sd', builtin.diagnostics, { desc = '[S]earch [D]iagnostics' })
+        end
+    },
+
+    -- Use fzf as sorter in Telescope. It's faster and supports fzf patterns such as 'exact matches.
+    {
+        'nvim-telescope/telescope-fzf-native.nvim',
+        build = 'make',
+        cond = vim.fn.executable 'make' == 1, -- Only load if `make` is available.
+        dependencies = { 'nvim-telescope/telescope.nvim' },
+        config = function() require('telescope').load_extension('fzf') end
+    },
+
+    -- Use "gc" to comment visual regions/lines.
+    {
+        'numToStr/Comment.nvim',
+        config = function() require('Comment').setup() end
+    },
+
+    -- Transparent background in Neovim. Does what it says on the tin.
+    {
+        'xiyaowong/nvim-transparent',
+        config = function() require("transparent").setup { enable = true } end
+    },
+
+    {
+        'cdelledonne/vim-cmake',
+        config = function()
+            vim.g.cmake_link_compile_commands = true -- Make symlink to compile_commands.json.
+            -- vim.keymap.set('', '<leader>cg', ':CMakeGenerate<cr>', { desc = '[C]Make [G]enerate' })
+            -- vim.keymap.set('', '<leader>cb', ':CMakeBuild<cr>', { desc = '[C]Make [B]uild' })
+            -- vim.keymap.set('', '<leader>cq', ':CMakeClose<cr>', { desc = '[C]Make [Q]uit' })
+            -- vim.keymap.set('', '<leader>cc', ':CMakeClean<cr>', { desc = '[C]Make [C]lean' })
+        end
+    },
+}
